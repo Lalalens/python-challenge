5493d78c8936d62de77afddeec2905b06fa4cda4
lalalens <lalagilbert@shelontas-mbp.myfiosgateway.com>
2023-04-15 14:03:50 -0400

    Adding files

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+# I'm inmporting my operating system to create a file path
+import os
+
+# Module for importing CSV files
+import csv
+
+# Module for reading CSV files
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv' )
