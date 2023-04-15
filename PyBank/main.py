21bafc8040874a67048f82a152643032050af5c7
lalalens <lalagilbert@shelontas-mbp.myfiosgateway.com>
2023-04-15 14:08:31 -0400

    Adding files

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,4 +5,10 @@ import os
 import csv
 
 # Module for reading CSV files
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv' )
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+# Reading the csv files
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    print(csvreader)
