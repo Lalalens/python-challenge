bd2b757c90589bee43cc059e0d7aea4ba9267d82
lalalens <lalagilbert@Shelontas-MacBook-Pro.local>
2023-04-19 02:29:39 -0400

    Update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,11 +4,8 @@ import os
 #import csv file
 import csv
 
-print("Election Results")
-print("------------------------")
-
 #Module for readinf CSV files
-csvpath = os.path.join('.', 'Resources', 'election_data.csv')
+csvpath = os.path.join('.', 'Resources', 'election_data.csv') # './Resources/election_data.csv'
 
 #reading CSV files
 with open(csvpath) as csvfile:
@@ -25,31 +22,44 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         # same as below just short hand total_votes = total_votes + 1
         total_votes += 1
-        candidate_name =row[2]
+        candidate_name = row[2]
         if candidate_name not in candidates:
             candidates[candidate_name] = 1
         else:
             candidates[candidate_name] += 1
 
-
-    print(f"Total Votes: {total_votes}")
-    print("------------------------")
-   
-
 #Percentage of votes and candidates who won
 max_votes = 0
 winner = ""
+
+print("Election Results")
+print("------------------------")
+
+print(f"Total Votes: {total_votes}")
+print("------------------------")
+
 for candidate, votes in candidates.items():
     percentage = (votes / total_votes) * 100
-    print(f"{candidate}: {percentage: .3f}% ({votes})")
+    print(f"{candidate}: {round(percentage, 3)}% ({votes})")
     
     #Find who won
     if votes > max_votes:
         max_votes = votes
         winner = candidate
-
+print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
+
+with open('./analysis/results.txt', 'w') as file:
+    file.write(("Election Results"))
+    file.write(("------------------------"))
+    file.write((f"Total Votes: {total_votes}"))
+    file.write(("------------------------"))
+    file.write((f"{candidate}: {round(percentage, 3)}% ({votes})"))
+    file.write(("-------------------------"))
+    file.write((f"Winner: {winner}"))
+    file.write(("-------------------------"))
+    
 
 
 
