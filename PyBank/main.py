8e511d37b89ef6fba1701550654d7fc3174ed1c4
lalalens <lalagilbert@shelontas-mbp.myfiosgateway.com>
2023-04-20 00:16:17 -0400

    Update

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,7 +14,6 @@ csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
-   # print(f"CSV Header: {csv_header}")
 
     #Set values
     total = 0
